67b5d397c50666a3699c48fc755ca10c985cb9ab
lgb2002 <cccavin12345@naver.com>
2019-01-01 18:35:40 +0900

    halo

--- main/urls.py
+++ main/urls.py
@@ -8,20 +8,20 @@ from main.forms import LoginForm
 urlpatterns = [
 	url(r'', include('social.apps.django_app.urls', namespace='social')),
 	url(r'', include('django.contrib.auth.urls', namespace='auth')),
-	url(r'^signup/$', views.signup, name='signup'),
-	url(r'^signup_ok/$', TemplateView.as_view(
+	url(r'^signup$', views.signup, name='signup'),
+	url(r'^signup_ok$', TemplateView.as_view(
         template_name='registration/signup_ok.html'
     ), name='signup_ok'),
-	url(r'^login/', auth_views.login, {
+	url(r'^login', auth_views.login, {
         'authentication_form': LoginForm
     	}, name='login_url'),
-	url(r'^logout/', auth_views.logout, {
-        'next_page': '/login/',
+	url(r'^logout', auth_views.logout, {
+        'next_page': '/login',
     	}, name='logout_url'),
 	url(r'^about/$', TemplateView.as_view(
         template_name='about.html'
     ), name='about'),
-    url(r'^summernote/', include('django_summernote.urls')),
+    url(r'^summernote', include('django_summernote.urls')),
     url(r'^$', views.home, name='home'),
     url(r'^join', views.join, name='join'),
    
