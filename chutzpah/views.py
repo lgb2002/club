4a581fc872c8811b0630cfe1ffd2742c9f80784c
lgb2002 <cccavin12345@naver.com>
2019-08-30 08:58:52 +0900

    test

--- chutzpah/views.py
+++ chutzpah/views.py
@@ -13,6 +13,22 @@ def rank(request):
 	ranking = UserRank.objects.all().order_by('user_point')
 	return render(request, 'chutzpah/game.html',{'ranking':ranking})
 
+'''
+try:
+	name1 = request.POST.get('name1')
+	try:
+		name_exist = UserRank.objects.get(user_name=name1)
+		return render(request, 'chutzpah/game.html',{'exist':1})
+	except:
+		return render(request, 'chutzpah/game.html',{'exist':0})
+except:
+	name = request.POST.get('name2')
+	point = request.POST.get('point2')
+	rank = UserRank(user_name=name,user_point=point)
+	rank.save()
+	ranking = UserRank.objects.all().order_by('user_point')
+	return render(request, 'chutzpah/game.html',{'ranking':ranking})
+	'''
 
 def game(request):
 	ranking = UserRank.objects.all().order_by('-user_point')
